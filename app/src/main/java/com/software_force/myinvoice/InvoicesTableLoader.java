da0d86581b10d2f39e43a06b071b2bd16e8b4add
matthewycso <matthewycso@yahoo.com.hk>
2015-10-05 14:58:39 +0800

    Use TableLayout for invoices

--- app/src/main/java/com/software_force/myinvoice/InvoicesTableLoader.java
+++ app/src/main/java/com/software_force/myinvoice/InvoicesTableLoader.java
@@ -3,6 +3,8 @@ package com.software_force.myinvoice;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -15,7 +17,7 @@ import java.util.List;
 
 
 /**
- * Created by Matthew So on 10/5/2015.
+ * Created by Matthew So on 10/5/textSize15.
  */
 public class InvoicesTableLoader {
 
@@ -35,6 +37,12 @@ public class InvoicesTableLoader {
         this.table.removeAllViewsInLayout();
         int flag = 1;
 
+        // int textSize =  (int) ( context.getResources().getDimension( R.dimen.text_size)/ context.getResources().getDisplayMetrics().density );
+        int textSize =  (int) ( context.getResources().getDimension( R.dimen.text_size) );
+        int textColor =  ContextCompat.getColor(context, R.color.colorrText);
+        int headerTextColor =  ContextCompat.getColor(context, R.color.colorHeaderText);
+        SimpleDateFormat dateFormat = new    SimpleDateFormat("yyyy-MM-dd");
+
         // when i=-1, loop will display heading of each column
         // then usually data will be display from i=0 to data.length()
         for (int i = -1; i < data.size(); i++) {
@@ -49,30 +57,38 @@ public class InvoicesTableLoader {
             if (flag == 1) {
 
                 TextView b3 = new TextView(this.context);
-                b3.setText("Id");
-                b3.setTextColor(Color.BLUE);
-                b3.setTextSize(15);
+                b3.setText(context.getString(R.string.invoice_id));
+                b3.setTextColor(headerTextColor);
+                b3.setTextSize(textSize);
                 tr.addView(b3);
 
                 TextView b4 = new TextView(this.context);
                 b4.setPadding(10, 0, 0, 0);
-                b4.setTextSize(15);
-                b4.setText("Date");
-                b4.setTextColor(Color.BLUE);
+                b4.setTextSize(textSize);
+                b4.setText(context.getString(R.string.invoice_date));
+                b4.setTextColor(headerTextColor);
                 tr.addView(b4);
 
                 TextView b5 = new TextView(this.context);
                 b5.setPadding(10, 0, 0, 0);
-                b5.setText("Member");
-                b5.setTextColor(Color.BLUE);
-                b5.setTextSize(15);
+                b5.setText(context.getString(R.string.invoice_member));
+                b5.setTextColor(headerTextColor);
+                b5.setTextSize(textSize);
                 tr.addView(b5);
+
+                TextView b6 = new TextView(this.context);
+                b6.setPadding(10, 0, 0, 0);
+                b6.setText(context.getString(R.string.invoice_net));
+                b6.setTextColor(headerTextColor);
+                b6.setTextSize(textSize);
+                tr.addView(b6);
+
                 this.table.addView(tr);
 
                 final View vline = new View(this.context);
                 vline.setLayoutParams(new
                         TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, 2));
-                vline.setBackgroundColor(Color.BLUE);
+                vline.setBackgroundColor(Color.YELLOW);
                 this.table.addView(vline); // add line below heading
                 flag = 0;
             } else {
@@ -82,25 +98,35 @@ public class InvoicesTableLoader {
                 TextView b = new TextView(this.context);
                 String str = String.valueOf(dataItem.getId());
                 b.setText(str);
-                b.setTextColor(Color.RED);
-                b.setTextSize(15);
+                b.setTextColor(textColor);
+                b.setTextSize(textSize);
+                b.setGravity(Gravity.CENTER_HORIZONTAL);
                 tr.addView(b);
 
                 TextView b1 = new TextView(this.context);
                 b1.setPadding(10, 0, 0, 0);
-                b1.setTextSize(15);
-                String str1 = new SimpleDateFormat().format(dataItem.getInvoiceDate());
+                b1.setTextSize(textSize);
+                String str1 = dateFormat.format(dataItem.getInvoiceDate());
                 b1.setText(str1);
-                b1.setTextColor(Color.WHITE);
+                b1.setTextColor(textColor);
                 tr.addView(b1);
 
                 TextView b2 = new TextView(this.context);
                 b2.setPadding(10, 0, 0, 0);
                 String str2 = dataItem.getMember();
                 b2.setText(str2);
-                b2.setTextColor(Color.RED);
-                b2.setTextSize(15);
+                b2.setTextColor(textColor);
+                b2.setTextSize(textSize);
                 tr.addView(b2);
+
+                TextView b3 = new TextView(this.context);
+                b3.setPadding(10, 0, 0, 0);
+                b3.setText(dataItem.getNet().toString());
+                b3.setTextColor(textColor);
+                b3.setTextSize(textSize);
+                b3.setGravity(Gravity.RIGHT);
+                tr.addView(b3);
+
 
                 this.table.addView(tr);
 
