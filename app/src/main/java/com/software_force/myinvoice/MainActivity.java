c6cb939b3cca6ff8bc805770b3644e3f69206a21
matthewycso <matthewycso@yahoo.com.hk>
2015-10-05 13:11:26 +0800

    Use TableLayout for invoices

--- app/src/main/java/com/software_force/myinvoice/MainActivity.java
+++ app/src/main/java/com/software_force/myinvoice/MainActivity.java
@@ -1,5 +1,7 @@
 package com.software_force.myinvoice;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -10,6 +12,7 @@ import android.os.Environment;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -17,15 +20,22 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.TableLayout;
+
+import com.software_force.myinvoice.models.Invoice;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
-public class MainActivity extends ListActivity {
+// public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
 
-    private ListView listView;
+    // private ListView listView;
+    private  TableLayout table;
     private InvoicesAdapter customAdapter;
-    private Cursor cursor;
+    // private Cursor cursor;
+    private List<Invoice> data;
     private InvoicesDataSource dataSource;
 
     @Override
@@ -33,12 +43,17 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = getListView();
+        // listView = getListView();
+        this.table =  (TableLayout)this.findViewById(R.id.invoices_table);
+
         this.dataSource = new InvoicesDataSource(this);
         this.dataSource.open();
-        cursor =  dataSource.getAllInvoicesAsCursor();
+        // cursor =  dataSource.getAllInvoicesAsCursor();
+        this.data = dataSource.getAllInvoices();
 
-        // this.dataSource.CreateSample();
+        if (this.dataSource.numberOfRows() == 0){
+            this.dataSource.CreateSample();
+        }
 
         String targetPath = "/mnt/extSdCard/Android/data/com.software_force.myinvoice";
 
@@ -63,17 +78,19 @@ public class MainActivity extends ListActivity {
 
             @Override
             public void run() {
-                customAdapter = new InvoicesAdapter(
-                        MainActivity.this,
-                        cursor,
-                        0);
-
-                listView.setAdapter(customAdapter);
+//                customAdapter = new InvoicesAdapter(MainActivity.this, cursor, 0);
+ //               listView.setAdapter(customAdapter);
+                InvoicesTableLoader loader = new InvoicesTableLoader( MainActivity.this, table, data);
+                loader.Build();
             }
 
         });
 
+
+
     }
+
+
 
     @Override
     protected void onResume() {
