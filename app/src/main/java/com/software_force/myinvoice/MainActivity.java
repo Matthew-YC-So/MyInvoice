073688f874640688ac61dff796cf8d8a258b02d7
matthewycso <matthewycso@yahoo.com.hk>
2015-10-05 18:02:48 +0800

    Use TableLayout for invoices

--- app/src/main/java/com/software_force/myinvoice/MainActivity.java
+++ app/src/main/java/com/software_force/myinvoice/MainActivity.java
@@ -1,26 +1,16 @@
 package com.software_force.myinvoice;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Intent;
-import android.database.Cursor;
 import android.media.MediaScannerConnection;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
+import android.view.MenuInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 import android.widget.TableLayout;
+import android.widget.Toast;
+
 
 import com.software_force.myinvoice.models.Invoice;
 
@@ -86,11 +76,31 @@ public class MainActivity extends Activity {
 
         });
 
-
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu );
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        int id = item.getItemId();
+        if (id == R.id.action_search)
+        {
+            Toast.makeText(getApplicationContext(), "SEARCH", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_add) {
+            Toast.makeText(getApplicationContext(), "ADD", Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            Toast.makeText(getApplicationContext(), "DEFAULT", Toast.LENGTH_SHORT).show();
+            return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onResume() {
