531d4509a20f4a1a2c9bddd21cf9615bf8564e47
matthewycso <matthewycso@gmail.com>
2015-10-06 23:45:22 +0800

    ActionBar fix

--- app/src/main/java/com/software_force/myinvoice/InvoiceEditActivity.java
+++ app/src/main/java/com/software_force/myinvoice/InvoiceEditActivity.java
@@ -1,31 +1,113 @@
 package com.software_force.myinvoice;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class InvoiceEditActivity extends AppCompatActivity {
 
     private  EditText edit_invoiceDate;
+    private java.text.DateFormat dateFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invoice_edit);
+
+        dateFormatter = android.text.format.DateFormat.getDateFormat(this);
+
         edit_invoiceDate = (EditText) findViewById(R.id.invoiceDate);
-
         setInvoiceDate(new Date());
-
+        ImageButton selectDate =  (ImageButton) findViewById(R.id.selectInvoiceDate);
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showStartDateDialog(v);
+            }
+        });
     }
 
     private void  setInvoiceDate(Date date){
-        java.text.DateFormat formatter = android.text.format.DateFormat.getDateFormat(this);
-
-        edit_invoiceDate.setText(formatter.format(date));
-
+        edit_invoiceDate.setText(dateFormatter.format(date));
     }
+
+    public void showStartDateDialog(View v){
+        SelectDateFragment dialogFragment = new SelectDateFragment();
+
+        int yy;
+        int mm;
+        int dd;
+
+        Calendar calender = Calendar.getInstance();
+        String dt = edit_invoiceDate.getText().toString();
+        try {
+            Date date = dateFormatter.parse(dt);
+            calender.setTime(date);
+        } catch (ParseException ignored) {
+        }
+
+        Bundle args = new Bundle();
+        args.putInt("year", calender.get(Calendar.YEAR));
+        args.putInt("month", calender.get(Calendar.MONTH));
+        args.putInt("day", calender.get(Calendar.DAY_OF_MONTH));
+        dialogFragment.setArguments(args);
+        /**
+         * Set Call back to capture selected date
+         */
+        dialogFragment.setCallBack(ondate);
+        dialogFragment.show(this.getFragmentManager(), "Date Picker");
+    }
+
+    DatePickerDialog.OnDateSetListener ondate = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            Calendar cal = Calendar.getInstance();
+            cal.set(Calendar.YEAR, year);
+            cal.set(Calendar.MONTH, monthOfYear);
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            Date dateRepresentation = cal.getTime();
+            setInvoiceDate(dateRepresentation);
+        }
+    };
+
+    // DatePicker dialog
+    static public class SelectDateFragment extends DialogFragment {
+
+        private DatePickerDialog.OnDateSetListener ondateSet;
+        private int year, month, day;
+
+        public SelectDateFragment() {
+        }
+
+        public void setCallBack(DatePickerDialog.OnDateSetListener ondate) {
+            this.ondateSet = ondate;
+        }
+
+        @Override
+        public void setArguments(Bundle args) {
+            super.setArguments(args);
+            year = args.getInt("year");
+            month = args.getInt("month");
+            day = args.getInt("day");
+        }
+
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return new DatePickerDialog(getActivity(), this.ondateSet, year, month, day);
+        }
+     }
 }
--- app/src/main/java/com/software_force/myinvoice/MainActivity.java
+++ app/src/main/java/com/software_force/myinvoice/MainActivity.java
@@ -120,5 +120,4 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
-
 }
