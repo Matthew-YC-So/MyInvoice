6fa8f8aa31b4308a2acce2cc8dfee5c7850081b4
matthewycso <matthewycso@gmail.com>
2015-10-07 22:27:27 +0800

    ActionBar fix

--- app/src/main/java/com/software_force/myinvoice/InvoiceEditActivity.java
+++ app/src/main/java/com/software_force/myinvoice/InvoiceEditActivity.java
@@ -5,6 +5,8 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -38,6 +40,13 @@ public class InvoiceEditActivity extends AppCompatActivity {
                 showStartDateDialog(v);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_invoice_edit, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     private void  setInvoiceDate(Date date){
